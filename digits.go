02ac995eeca77d1849a1af57db163d106d6df7a8
komachihashimoto <joujinameko@gmail.com>
2024-01-09 12:05:08 +0900

    改行

--- digits.go
+++ digits.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	a := 5.2
 	b := 7.4
-	fmt.Printf("a+b=%.2f",a+b)
-	fmt.Printf("a-b=%.2f",a-b)
-	fmt.Printf("a*b=%.2f",a*b)
-	fmt.Printf("a/b=%.4f",a/b)
+	fmt.Printf("a+b=%.2f\n",a+b)
+	fmt.Printf("a-b=%.2f\n",a-b)
+	fmt.Printf("a*b=%.2f\n",a*b)
+	fmt.Printf("a/b=%.4f\n",a/b)
 }
